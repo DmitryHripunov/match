1fe6cdcd03ae34796d349fe978238a9f11ce9a46
Dmitry Hripunov <s_pektr_e@mail.ru>
2021-08-06 17:26:27 +0300

    opd 0.0.4w

--- src/common.blocks/cursor/cursor.js
+++ src/common.blocks/cursor/cursor.js
@@ -1,12 +1,16 @@
 import { gsap } from 'gsap';
 
-// gsap.set('.cursor__pointer', { xPersent: -50, yPersent: -50 });
-// gsap.set('.cursor__follower', { xPersent: -50, yPersent: -50 });
+document.addEventListener('DOMContentLoaded', () => {
+  if (window.innerWidth > 1024) {
+    document.body.style.cursor = 'none';
+  }
 
-const cursor = document.querySelector('.cursor__pointer');
-const follow = document.querySelector('.cursor__follower');
+  const cursor = document.querySelector('.cursor__pointer');
+  const follow = document.querySelector('.cursor__follower');
 
-window.addEventListener('mousemove', (e) => {
-  gsap.to(cursor, 0, { x: e.clientX, y: e.clientY });
-  gsap.to(follow, 0.5, { x: e.clientX, y: e.clientY });
+  window.addEventListener('mousemove', (e) => {
+    gsap.to(cursor, 0, { x: e.clientX, y: e.clientY });
+    gsap.to(follow, 0.3, { x: e.clientX, y: e.clientY });
+  });
 });
+
