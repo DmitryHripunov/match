0bf478916568ad6e6ccb83f311f7939c8744baae
Dmitry Hripunov <s_pektr_e@mail.ru>
2021-09-06 09:26:15 +0300

    fix cursor in center

--- src/common.blocks/cursor/cursor.js
+++ src/common.blocks/cursor/cursor.js
@@ -1,66 +1,65 @@
 import { gsap } from 'gsap';
 
 document.addEventListener('DOMContentLoaded', () => {
-  if (window.innerWidth > 1024) {
-    document.body.style.cursor = 'none';
+  // if (window.innerWidth > 1024) {
+  // document.body.style.cursor = 'none';
 
-    const cursor = document.querySelector('.cursor__pointer');
-    const follow = document.querySelector('.cursor__follower');
+  const cursor = document.querySelector('.cursor__pointer');
+  const follow = document.querySelector('.cursor__follower');
 
-    window.addEventListener('mousemove', (e) => {
-      gsap.to(cursor, 0, { x: e.clientX, y: e.clientY });
-      gsap.to(follow, 0.3, { x: e.clientX, y: e.clientY });
+  window.addEventListener('mousemove', (e) => {
+    gsap.to(cursor, 0, { x: e.clientX, y: e.clientY });
+    gsap.to(follow, 0.3, { x: e.clientX, y: e.clientY });
+  });
+
+  [...document.querySelectorAll('a')].forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      follow.classList.add('is-hover');
     });
 
-    [...document.querySelectorAll('a')].forEach((item) => {
-      item.addEventListener('mouseenter', () => {
-        follow.classList.add('is-hover');
-      });
+    item.addEventListener('mouseleave', () => {
+      follow.classList.remove('is-hover');
+    });
+  });
 
-      item.addEventListener('mouseleave', () => {
-        follow.classList.remove('is-hover');
-      });
+  [...document.querySelectorAll('button')].forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      follow.classList.add('is-hover');
     });
 
-    [...document.querySelectorAll('button')].forEach((item) => {
-      item.addEventListener('mouseenter', () => {
-        follow.classList.add('is-hover');
-      });
+    item.addEventListener('mouseleave', () => {
+      follow.classList.remove('is-hover');
+    });
+  });
 
-      item.addEventListener('mouseleave', () => {
-        follow.classList.remove('is-hover');
-      });
+  [...document.querySelectorAll('input')].forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      follow.classList.add('is-hover');
     });
 
-    [...document.querySelectorAll('input')].forEach((item) => {
-      item.addEventListener('mouseenter', () => {
-        follow.classList.add('is-hover');
-      });
+    item.addEventListener('mouseleave', () => {
+      follow.classList.remove('is-hover');
+    });
+  });
 
-      item.addEventListener('mouseleave', () => {
-        follow.classList.remove('is-hover');
-      });
+  [...document.querySelectorAll('label')].forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      follow.classList.add('is-hover');
     });
 
-    [...document.querySelectorAll('label')].forEach((item) => {
-      item.addEventListener('mouseenter', () => {
-        follow.classList.add('is-hover');
-      });
+    item.addEventListener('mouseleave', () => {
+      follow.classList.remove('is-hover');
+    });
+  });
 
-      item.addEventListener('mouseleave', () => {
-        follow.classList.remove('is-hover');
-      });
+  [...document.querySelectorAll('textarea')].forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+      follow.classList.add('is-hover');
     });
 
-    [...document.querySelectorAll('textarea')].forEach((item) => {
-      item.addEventListener('mouseenter', () => {
-        follow.classList.add('is-hover');
-      });
-
-      item.addEventListener('mouseleave', () => {
-        follow.classList.remove('is-hover');
-      });
+    item.addEventListener('mouseleave', () => {
+      follow.classList.remove('is-hover');
     });
-  }
+  });
+  // }
 });
-
